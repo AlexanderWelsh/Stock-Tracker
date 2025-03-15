29c9048e57b3c17ee5317100d36a9693b3b1efee
Alexander Welsh <alexander.cole.welsh@gmail.com>
2025-03-14 23:31:48 -0400

    Add files via upload

---
+++ Program 4/Program 4/Product.cs
@@ -0,0 +1,77 @@
+﻿//Program 4
+//CIS-199-50
+//Due 12/4/2023
+//By: A1029
+//This file creates a simple Product class capable of tracking the product supplier name, product name, product name, product type, product price, aisle number, and if it is in stock or not
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    public class Product
+    {
+        //creates each of the variables
+        public string supplierName;
+        public string productName;
+        public int productID;
+        public string productType;
+        public double productPrice;
+        public int aisleLocation;
+        private bool inStock;
+
+        //creates alternate variables to be used inside of the class
+        public Product(string supplierName, string productName, int productID, string productType, double productPrice, int aisleLocation)
+        {
+            SupplierName = supplierName;
+            ProductName = productName;
+            ProductID = productID;
+            ProductType = productType;
+            ProductPrice = productPrice;
+            Aisle = aisleLocation;
+            inStock = false;
+        }
+
+        public string SupplierName { get; set; }
+        public string ProductName { get; set; }
+
+        //sets the minimum product id to 100000 and max to 999999. Defalt 0
+        public int ProductID { get => productID; set => productID = (value >= 100000 && value <= 999999) ? value : 0; }
+
+        public string ProductType { get; set; }
+
+        //sets the minimum product price to greater then 0 and the defalut value to 0
+        public double ProductPrice { get => productPrice; set => productPrice = (value >= 0) ? value : 0; }
+
+        //sets the minimum aisle value to 1 and max to 20. Defalt is set to 0.
+        public int Aisle { get => aisleLocation; set => aisleLocation = (value >= 1 && value <= 20) ? value : 0; }
+
+        //sets the instock variable to true
+        public void InStock()
+        {
+            inStock = true;
+        }
+
+        //sets the OutOfStock variable to false
+        public void OutOfStock()
+        {
+            inStock = false;
+        }
+
+        //returns a bool value if the item is in stock or not
+        public bool IsOutOfStock()
+        {
+            return !inStock;
+        }
+
+        //prints all variables as a string, overriding all other types
+        public override string ToString()
+        {
+            return $"Supplier Name: {SupplierName}\nProduct Name: {ProductName}\nProduct ID: {ProductID}\nProduct Type: {ProductType}\nProduct Price: {ProductPrice:C}\nAisle: {Aisle}\nIn Stock: {(inStock ? "Yes" : "No")}";
+        }
+
+    }
+}
---
+++ Program 4/Program 4/Program.cs
@@ -0,0 +1,74 @@
+﻿//Program 4
+//CIS-199-50
+//Due 12/4/2023
+//By: A1029
+//This file creates a simple test application class that creates an array of product objects and prints them first with their original data and then changes the data and prints the in stock units.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    internal class Program
+    {
+        //creates an object that will print all data found in the product class
+        static void PrintData(Product[] products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.ToString() + "\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //creates each of the product objects and assigns it a variable  
+            Product product1 = new Product("PepsiCo", "Gatorade", 675134, "Beverage", 3.50, 4);
+            Product product2 = new Product("PepsiCo", "Gatorade", 675134, "Beverage", 3.50, 4);
+            Product product3 = new Product("PepsiCo", "Gatorade", 675134, "Beverage", 3.50, 4);
+            Product product4 = new Product("PepsiCo", "Gatorade", 675134, "Beverage", 3.50, 4);
+            Product product5 = new Product("PepsiCo", "Gatorade", 675134, "Beverage", 3.50, 4);
+
+            //creates five product objects and organizes them as lists of their attributes 
+            Product[] products = new Product[]
+            {
+                new Product("PepsiCo", "Gatorade", 675134, "Beverage", 3.50, 4),
+                new Product("Kellogg's", "Corn Flakes", 123456, "Cereal", 2.99, 8),
+                new Product("Coca-Cola", "Coke", 789012, "Beverage", 1.99, 12),
+                new Product("Campbell's", "Tomato Soup", 445566, "Canned Goods", 2.75, 3),
+                new Product("Kraft", "Macaroni and Cheese", 990011, "Pasta", 1.99, 7),
+            };
+
+            Console.WriteLine("List of Products We Sell:\n");
+            Console.WriteLine("---------------------------");
+            PrintData(products);
+
+            //makrs if each object is in stock and therefore gets printed at the end stage or not
+            products[0].InStock();
+            products[1].InStock();
+            products[2].OutOfStock();
+            products[3].InStock();
+            products[4].OutOfStock();
+
+            //changes items in the lists before they are printed as results
+            products[0].ProductPrice = 4.00;
+            products[2].Aisle = 12;
+            products[3].ProductPrice = 3.20;
+            products[4].ProductName = "Mac and Cheese";
+
+
+            Console.WriteLine("After Changes\n");
+            Console.WriteLine("---------------------------");
+
+            //only prints items that are in stock
+            foreach (var product in products)
+            {
+                if (!product.IsOutOfStock())
+                {
+                    Console.WriteLine(product.ToString() + "\n");
+                }
+            }
+        }
+    }
+}
